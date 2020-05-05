d27c37e650f3e7181108231060a230adc8c1f536
T1916 <63775715+T1916@users.noreply.github.com>
2020-05-05 15:49:54 +0530

    Add files via upload

---
+++ Assets/Scripts/DestroyEnemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DestroyEnemy : MonoBehaviour
+{
+    public GameObject enemy;
+    public Text scoretext;
+    public Text timer;
+    public countdowncontroller countdowncontroller;
+    
+
+    private int score = 0;
+    private float timeElapsed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("begingame", 3);
+        
+    }
+
+    void begingame()
+    {
+        
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Destroy(enemy.gameObject);
+                ScoreSys.ScoreValue += 1;
+                timeElapsed = Time.time - countdowncontroller.returntime();
+                timer.text = "Time: " + timeElapsed;
+
+            }
+        
+       
+        
+    }
+
+
+}
